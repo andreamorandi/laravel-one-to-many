56f1c056d8b6864787bf9b96946eaaf12ae9e7f9
andreamorandi <morandiandrearocco@gmail.com>
2023-01-17 10:09:34 +0100

    Correzione Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -17,7 +17,7 @@ class Project extends Model
         return Str::slug($title, '-');
     }
 
-    public function category()
+    public function type()
     {
         return $this->belongsTo(Type::class);
     }
